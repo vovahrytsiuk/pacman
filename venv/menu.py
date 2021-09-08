ee52d6114ddf9b0abbb6bd10b1edb32d3866a761
vovahrytsiuk <vovaoffonua@gmail.com>
2021-09-08 21:45:28 +0300

    add events handler for menu bar

--- venv/menu.py
+++ venv/menu.py
@@ -1,4 +1,8 @@
+import pygame
+
 class Menu(object):
+    # state id display current item on menu bar
+    state = 0
     # initialize menu class
     def __init__(self):
         # menu color
@@ -9,3 +13,14 @@ class Menu(object):
         self.items = None
         # font for displaying menu
         self.font = None
+
+    def handle_events(self, event):
+        # here i gonna process K_DOWN and K_UP to communicate with menu bar
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                if self.state > 0:
+                    self.state += -1
+            elif event.key == pygame.K_DOWN:
+                if self.state < len(self.items) - 1:
+                    self.state += 1
+
